b8cea9492abcd1ce9afa8b1cd18435cac287d188
Lucas Bento Tosta <166650035+LBTosta@users.noreply.github.com>
2024-05-03 11:10:26 -0300

    Add files via upload

---
+++ src/javascript/script.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+    $('.content').hide();
+
+    $('.tab-btn').click(function() {
+        var tab = $(this);
+
+        $('.tab-btn').removeClass('active');
+        tab.addClass('active');
+
+        $('.content').hide();
+
+        var contentId = tab.attr('content-id');
+        $('#' + contentId).show();
+    });
+
+    var currentActiveTab = $('.tab-btn.active');
+    currentActiveTab.click();
+});
